d5fea66ab13cb1ab4c1987f2ba38ed2ba47a772b
taehee <wonderfulhuman@naver.com>
2021-09-25 13:41:00 +0900

    Add: Use Email-Validation with react-hook-form

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,9 +1,50 @@
 import React from "react";
+import "./styles.css";
+import {Link} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
+
 
 function RegisterPage() {
+
+  const {register, watch, formState: {errors} } = useForm({mode: "onChange"});
+
+  console.log(watch("email"))
   return (
-    <div>
-      RegisterPage
+    <div className="auth-wrapper">
+      <div style={{textAlign: 'center'}}>
+        <h3>Register</h3>
+      </div>
+      <form>
+        <label>Email</label>
+        <input
+          name="email"
+          type="email"
+          {...register("email", {required: true, maxLength: 10})}
+        />
+        {console.log(errors)}
+        {errors.email && <p>This field is required</p>}
+        <label>Name</label>
+        <input
+          name="name"
+        />
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+        />
+        <label>Password Confirm</label>
+        <input
+          name="password_confirm"
+          type="password"
+        />
+        <input type="submit"/>
+      </form>
+      <Link
+        style={{
+          color: 'gray',
+          textDecoration: 'none',
+        }}
+        to="login">이미 아이디가 있다면...</Link>
     </div>
   )
 }

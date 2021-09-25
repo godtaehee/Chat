64d19df1ff4d698a1064ad6017705a5643fa994d
taehee <wonderfulhuman@naver.com>
2021-09-26 01:59:13 +0900

    Add: Prettier

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,21 +1,24 @@
-import React, {useRef, useState} from "react";
-import "./styles.css";
-import {Link} from 'react-router-dom';
-import {useForm} from 'react-hook-form';
-import firebase from "../../firebase";
-import {getAuth, createUserWithEmailAndPassword} from "@firebase/auth";
+import React, { useRef, useState } from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import firebase from '../../firebase';
+import { getAuth, createUserWithEmailAndPassword } from '@firebase/auth';
 
 function RegisterPage() {
-
-
   const auth = getAuth(firebase);
 
-  const [errorFromSubmit, setErrorFromSubmit] = useState("");
+  const [errorFromSubmit, setErrorFromSubmit] = useState('');
 
-  const {register, watch, formState: {errors}, handleSubmit } = useForm();
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   const password = useRef();
-  password.current = watch("password");
+  password.current = watch('password');
 
   const onSubmit = async (data) => {
     try {
@@ -23,16 +26,15 @@ function RegisterPage() {
         auth,
         data.email,
         data.password
-      )
-    }catch (e) {
+      );
+    } catch (e) {
       setErrorFromSubmit(e.message);
     }
-
-  }
+  };
 
   return (
     <div className="auth-wrapper">
-      <div style={{textAlign: 'center'}}>
+      <div style={{ textAlign: 'center' }}>
         <h3>Register</h3>
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -40,53 +42,69 @@ function RegisterPage() {
         <input
           name="email"
           type="email"
-          {...register("email", {required: true, pattern: /^\S+@\S+$/i })}
+          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
         />
         {errors.email && <p>This field is required</p>}
 
         <label>Name</label>
         <input
           name="name"
-          {...register("name", {required: true, maxLength: 10 })}
+          {...register('name', { required: true, maxLength: 10 })}
         />
-        {errors.name && errors.name.type === 'required' && <p>This name field is required</p>}
-        {errors.name && errors.name.type === 'maxLength' && <p>Your input exceed maxium length</p>}
+        {errors.name && errors.name.type === 'required' && (
+          <p>This name field is required</p>
+        )}
+        {errors.name && errors.name.type === 'maxLength' && (
+          <p>Your input exceed maxium length</p>
+        )}
 
         <label>Password</label>
         <input
           name="password"
           type="password"
-          {...register("password", {required: true, minLength: 6})}
+          {...register('password', { required: true, minLength: 6 })}
         />
 
-        {errors.password && errors.password.type === 'required' && <p>This password field is required</p>}
-        {errors.password && errors.password.type === 'minLength' && <p>Password must have at least 6 characters</p>}
+        {errors.password && errors.password.type === 'required' && (
+          <p>This password field is required</p>
+        )}
+        {errors.password && errors.password.type === 'minLength' && (
+          <p>Password must have at least 6 characters</p>
+        )}
 
         <label>Password Confirm</label>
         <input
           name="password_confirm"
           type="password"
-          {...register("password_confirm", {
-            required:true,
-            validate: (value) => value === password.current
-            }
-          )}
+          {...register('password_confirm', {
+            required: true,
+            validate: (value) => value === password.current,
+          })}
         />
-        {errors.password_confirm && errors.password_confirm.type === 'required' && <p>This password_confirm field is required</p>}
-        {errors.password_confirm && errors.password_confirm.type === 'validate' && <p>The password do not match</p>}
+        {errors.password_confirm &&
+          errors.password_confirm.type === 'required' && (
+            <p>This password_confirm field is required</p>
+          )}
+        {errors.password_confirm &&
+          errors.password_confirm.type === 'validate' && (
+            <p>The password do not match</p>
+          )}
 
         {errorFromSubmit && <p>{errorFromSubmit}</p>}
 
-        <input type="submit"/>
+        <input type="submit" />
       </form>
       <Link
         style={{
           color: 'gray',
           textDecoration: 'none',
         }}
-        to="login">이미 아이디가 있다면...</Link>
+        to="login"
+      >
+        이미 아이디가 있다면...
+      </Link>
     </div>
-  )
+  );
 }
 
 export default RegisterPage;

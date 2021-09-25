362cd8988fffc34b3bac6a97918ab96cca7a2314
taehee <wonderfulhuman@naver.com>
2021-09-26 01:42:51 +0900

    Add: Firebase Auth with Version 9

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,42 +1,82 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import "./styles.css";
 import {Link} from 'react-router-dom';
 import {useForm} from 'react-hook-form';
-
+import firebase from "../../firebase";
+import {getAuth, createUserWithEmailAndPassword} from "@firebase/auth";
 
 function RegisterPage() {
 
-  const {register, watch, formState: {errors} } = useForm({mode: "onChange"});
 
-  console.log(watch("email"))
+  const auth = getAuth(firebase);
+
+  const [errorFromSubmit, setErrorFromSubmit] = useState("");
+
+  const {register, watch, formState: {errors}, handleSubmit } = useForm();
+
+  const password = useRef();
+  password.current = watch("password");
+
+  const onSubmit = async (data) => {
+    try {
+      let createdUser = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
+    }catch (e) {
+      setErrorFromSubmit(e.message);
+    }
+
+  }
+
   return (
     <div className="auth-wrapper">
       <div style={{textAlign: 'center'}}>
         <h3>Register</h3>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Email</label>
         <input
           name="email"
           type="email"
-          {...register("email", {required: true, maxLength: 10})}
+          {...register("email", {required: true, pattern: /^\S+@\S+$/i })}
         />
-        {console.log(errors)}
         {errors.email && <p>This field is required</p>}
+
         <label>Name</label>
         <input
           name="name"
+          {...register("name", {required: true, maxLength: 10 })}
         />
+        {errors.name && errors.name.type === 'required' && <p>This name field is required</p>}
+        {errors.name && errors.name.type === 'maxLength' && <p>Your input exceed maxium length</p>}
+
         <label>Password</label>
         <input
           name="password"
           type="password"
+          {...register("password", {required: true, minLength: 6})}
         />
+
+        {errors.password && errors.password.type === 'required' && <p>This password field is required</p>}
+        {errors.password && errors.password.type === 'minLength' && <p>Password must have at least 6 characters</p>}
+
         <label>Password Confirm</label>
         <input
           name="password_confirm"
           type="password"
+          {...register("password_confirm", {
+            required:true,
+            validate: (value) => value === password.current
+            }
+          )}
         />
+        {errors.password_confirm && errors.password_confirm.type === 'required' && <p>This password_confirm field is required</p>}
+        {errors.password_confirm && errors.password_confirm.type === 'validate' && <p>The password do not match</p>}
+
+        {errorFromSubmit && <p>{errorFromSubmit}</p>}
+
         <input type="submit"/>
       </form>
       <Link
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,11 @@
-import {initializeApp } from "firebase/firebase-app";
-import  {getAnalytics} from "firebase/firebase-analytics";
-import "firebase/auth";
-import "firebase/database";
-import "firebase/storage";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDekc0uiRvrTXVkEUVx3i56Avvm4519ds4",
   authDomain: "react-firebase-chat-app-be628.firebaseapp.com",
@@ -15,5 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+
+
+export default firebase;
